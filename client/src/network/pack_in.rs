98ae1272250a0b741abb96e3573b56bcada4aa3c
broken <broken>
2025-02-07 19:05:17 +0100

    hopefully fix

--- client/src/network/pack_in.rs
+++ client/src/network/pack_in.rs
@@ -196,7 +196,6 @@ impl Network {
                 Server => {
                     self.leaf_types.insert(id, None);
                     self.paths_to_leafs.insert(id, None);
-                    self.send_message(Message::ReqServerType, id, None);
                     if let Some(stream) = &mut self.frontend_stream {
                         let _ = send_message(stream, UpdateChatRoom(id, None, Some(true)));
                     }
@@ -205,6 +204,9 @@ impl Network {
 
             let _ = self.add_path(&path, true);
             self.update_unreachable_paths();
+            if node_type == Server {
+                self.send_message(Message::ReqServerType, id, None);
+            }
             self.check_queued(id);
         }
     }
