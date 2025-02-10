669f17d76f8c04216b8d9fdf7ecc379a5bf3197a
broken <broken>
2025-02-10 13:01:01 +0100

    add macos support

--- tui_common/src/sys.rs
+++ tui_common/src/sys.rs
@@ -1,10 +1,20 @@
 use std::process::{Command, Stdio};
 
+#[cfg(target_os = "linux")]
 pub fn open_terminal_with_command(command: &str) -> std::io::Result<()> {
     Command::new("terminator")
         .arg("-e")
         .arg(command)
         .stdout(Stdio::piped())
+        .spawn()?;
+
+    Ok(())
+}
+
+#[cfg(target_os = "macos")]
+pub fn open_terminal_with_command(command: &str) -> std::io::Result<()> {
+    Command::new("open")
+        .args(&["-a", "Terminal", command])
         .spawn()?;
 
     Ok(())
